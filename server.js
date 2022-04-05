70684aa66022c036bfc37d1f32650756ad4faf2d
mahodiatik <mahodiatikshuvo@gmail.com>
2022-04-06 03:14:25 +0600

    finished update delete and insert

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+
+app.use(express.json())
+const userRouter = require('./routes/user')
+app.use('/user', userRouter)
+
+mongoose.connect(process.env.DB_URL);
+
+const db= mongoose.connection
+db.on('error',(error) => console.error(error))
+db.once('open',()=> console.log('Connected to database'))
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
